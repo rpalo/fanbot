9719e0b24e7ecc42e8354bb6895fb61c4f3018a2
Ryan Palo <ryan@thepalos.com>
2017-05-04 06:50:40 -0700

    Fix bug when target compliments him/herself

--- fanbot/fanbot.py
+++ fanbot/fanbot.py
@@ -112,6 +112,7 @@ class Fanbot:
         if new_messages:
             other_requestors = [message["author"].name for message in new_messages
                                 if message["author"].screen_name != "Tpaylo"]
+            requestor_text = ""
             if other_requestors:
                 requestor_text = "from {}".format(", ".join(other_requestors))
             self.post_compliment(extra_text=requestor_text)
